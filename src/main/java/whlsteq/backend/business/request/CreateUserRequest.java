708384f7515be59fde4ece30d2ef5277df9e4b08
Whlsteq/UtkuC <utkucanal@whlsteq.co>
2024-02-02 07:39:23 +0300

    add size error message

--- src/main/java/whlsteq/backend/business/request/CreateUserRequest.java
+++ src/main/java/whlsteq/backend/business/request/CreateUserRequest.java
@@ -12,21 +12,21 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateUserRequest {
-    @NotNull(message = "Name cannot be null")
-    @NotBlank(message = "Name cannot be blank")
+    @NotNull(message = "Name cannot be null or blank")
+    @NotBlank(message = "Name cannot be null or blank")
     private String name;
 
-    @NotNull(message = "Surname cannot be null")
-    @NotBlank(message = "Name cannot be blank")
+    @NotNull(message = "Surname cannot be null or blank")
+    @NotBlank(message = "Surname cannot be null or blank")
     private String surname;
 
-    @NotNull(message = "Email cannot be null")
-    @NotBlank(message = "Email cannot be blank")
+    @NotNull(message = "Email cannot be null or blank")
+    @NotBlank(message = "Email cannot be null or blank")
     @Email
     private String email;
 
-    @NotNull(message = "Password cannot be null")
-    @NotBlank(message = "Password cannot be blank")
+    @NotNull(message = "Password cannot be null or blank")
+    @NotBlank(message = "Password cannot be null or blank")
     @Size(min=8,max = 16, message = "Size must be between 8 and 16")
     private String password;
 }
